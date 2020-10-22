6be80f4bf08be35b3a74c7749757af5a0f7c4441
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2020-10-22 14:49:45 +1100

    maj 22/10/2020

---
+++ script.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Form from './vue/Form';
+
+new Vue({
+    render: createElement => createElement(Form)
+}).$mount('#app');
---
+++ vue/Form.vue
@@ -0,0 +1,41 @@
+<template>
+  <form class="creditcard">
+      <div class="form_cc">
+        <label for="name">Name</label><br>
+        <input class="input" type="text" name="cc_name"><br>
+        <label for="cc_number">Number</label><br>
+        <input class="input" @input="event => number=event.target.value" v-bind:value="number | cardNumberFormat" type="text" name="cc_number"><br>
+        
+        <div class="key_cc">
+        <label for="name">CCV2</label><br>
+        <input class="input" type="text" name="cc_cvv">
+        <label for="name">Expiration Date</label><br>
+        <input class="input" type="text" name="cc_expiration"><br>
+        </div>
+        
+        <input type="submit" value="Pay">
+      </div>
+  </form>
+</template>
+
+<script>
+const creditcards = require('creditcards');
+
+export default {
+  name: 'Form',
+  filters: {
+    cardNumberFormat: function(value) {
+      return creditcards.card.format(creditcards.card.parse(value));
+    },
+  },
+  data() {
+    return {
+      type: '',
+      name: '',
+      number: '',
+      expiration: '',
+      cvv: '',
+    };
+  },
+};
+</script>
